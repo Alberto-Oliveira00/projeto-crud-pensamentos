1471091f4f48e4d7b84e3b904060e78a42f2e702
Alberto Oliveira <alberto.olv00@gmail.com>
2025-01-16 15:15:03 -0300

    Update js
    Adicionando DOM ao elementos e configurando API para receber os comandos da página

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value.trim()
+    const conteudo = document.getElementById("pensamento-conteudo").value.trim()
+    const autoria = document.getElementById("pensamento-autoria").value.trim()
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria })
+        ui.renderizarPensamentos()
+    } catch (error) {
+        alert("Erro ao salvar pensamentos")
+    }
+}
