8643e7ceaeb679a6c9a3e2df44d132d37ddddf82
Alberto Oliveira <alberto.olv00@gmail.com>
2025-01-24 19:48:35 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const URL_BASE = "http://localhost:3000"
+const URL_BASE = "https://alberto-oliveira00.github.io/projeto-crud-pensamentos"
 
 const converterStringParaData = (dataString) => {
     const [ano, mes, dia] = dataString.split("-")
